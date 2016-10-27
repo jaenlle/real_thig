44fd93ba9ae0d51f72489eb0d50faded3c440f39
Julio Aenlle <gmg8r251@ufl.edu>
2016-10-27 10:02:35 -0400

    Add container to header tag class

--- header.php
+++ header.php
@@ -23,7 +23,7 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'real_thig' ); ?></a>
 
-	<header id="masthead" class="site-header" role="banner">
+	<header id="masthead" class=" container site-header" role="banner">
                 <?php
                     wp_nav_menu( array(
                         'menu'              => 'top-menu',
