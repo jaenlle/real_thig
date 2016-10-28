f5ba22088389c5bb42a16a6ecaf30ac6bceef63c
Julio Aenlle <gmg8r251@ufl.edu>
2016-10-28 00:34:59 -0400

    Add global logo styling

--- header.php
+++ header.php
@@ -38,36 +38,38 @@
                     );
                 ?>
 
-		<nav id="site-navigation" class="navbar navbar-default" role="navigation">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="">
-                        <!-- Brand and toggle get grouped for better mobile display -->
-                        <div class="navbar-header">
-                          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                            <span class="sr-only">Toggle navigation</span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                          </button>
-                        </div>
-                            <?php
-                            wp_nav_menu( array(
-                                'menu'              => 'primary',
-                                'theme_location'    => 'primary',
-                                'depth'             => 2,
-                                'container'         => 'div',
-                                'container_class'   => 'collapse navbar-collapse',
-                                'container_id'      => 'bs-example-navbar-collapse-1',
-                                'menu_class'        => 'nav navbar-nav navbar-right',
-                                'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
-                                'walker'            => new wp_bootstrap_navwalker())
-                            );
-                            ?>
-                    </div>
-                </div>
-            </div>
-		</nav><!-- #site-navigation -->
+            <nav id="site-navigation" class="navbar navbar-default" role="navigation">
+                            <div class="container-fluid">
+                                <div class="row">
+                                    <div class="">
+                                        <!-- Brand and toggle get grouped for better mobile display-->
+                                        <div class="navbar-header">
+                                              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                                                <span class="sr-only">Toggle navigation</span>
+                                                <span class="icon-bar"></span>
+                                                <span class="icon-bar"></span>
+                                                <span class="icon-bar"></span>
+                                              </button>
+                                              <a href="http://localhost/realthig/"><img id="navlogo" src="http://localhost/realthig/wp-content/uploads/2016/10/thig_horizontal.png" alt="Tower Hill Insurance | Florida Homeowners Insurance">
+                                              </a>
+                                        </div>
+                                            <?php
+                                            wp_nav_menu( array(
+                                                'menu'              => 'primary',
+                                                'theme_location'    => 'primary',
+                                                'depth'             => 2,
+                                                'container'         => 'div',
+                                                'container_class'   => 'collapse navbar-collapse',
+                                                'container_id'      => 'bs-example-navbar-collapse-1',
+                                                'menu_class'        => 'nav navbar-nav navbar-right',
+                                                'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
+                                                'walker'            => new wp_bootstrap_navwalker())
+                                            );
+                                            ?>
+                                    </div>
+                                </div>
+                            </div>
+                        </nav><!--#site-navigation -->
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
