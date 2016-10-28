19e21ddf1171a92b1557840f7e4a513ea13c4b29
Julio Aenlle <gmg8r251@ufl.edu>
2016-10-27 23:00:02 -0400

    Add code for responsive global menu incl hmbrgr

--- header.php
+++ header.php
@@ -39,6 +39,18 @@
                 ?>
 
 		<nav id="site-navigation" class="navbar navbar-default" role="navigation">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="">
+                        <!-- Brand and toggle get grouped for better mobile display -->
+                        <div class="navbar-header">
+                          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                          </button>
+                        </div>
                             <?php
                             wp_nav_menu( array(
                                 'menu'              => 'primary',
